6683c0cd905cfb1d29343a84b682cd178e4b7ee0
Erik Huang <erikhuangdrums@gmail.com>
2021-02-27 21:52:27 -0800

    commit tree 92df6b6a7ca1946754b66c5fab92244a3d471fc7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+const mapToNegativize = arr => arr.map(n => -(n));
+const mapToNoChange = arr => arr.map(n => n);
+const mapToDouble = arr => arr.map(n => n * 2);
+const mapToSquare = arr => arr.map(n => n * n);
+
+const reduceToTotal = (arr, n=0) => {
+   return arr.reduce((s, v) => { return s + v }, n);
+}
+
+const reduceToAllTrue = arr => arr.every(b => !!b)
+
+const reduceToAnyTrue = arr => arr.some(b => !!b)
